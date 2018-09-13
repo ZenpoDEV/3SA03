8dd473876cfd8beb33af4b5bdf10596c52602ced
ZenpoDEV <krittamet0404@gmail.com>
2018-09-13 11:46:57 +0700

    Game logic

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import CharacterCard from './CharacterCard.js';
+import _ from 'lodash';
+const prepareStateFromWord = (given_word) => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return {
+        word,
+        chars,
+        attempt: 1,
+        guess: [],
+        completed: false
+    }
+}
 
 export default class WordCard extends
     Component {
@@ -10,11 +22,32 @@ export default class WordCard extends
             attempt: '',
         }
     }
-    activationHandler = c => { console.log(`${c} has been activated.`) }
+    componentWillMount(){
+        let data = prepareStateFromWord(this.props.value);
+        this.setState({
+            word:data.word,
+            chars: data.chars,
+            attempt: data.attempt,
+            guess: data.guess,
+            completed: data.completed,
+        })
+    }
+    activationHandler = (c) => {
+        let guess = [...this.state.guess, c]
+        this.setState({ guess })
+        if (guess.length == this.state.chars.length) {
+            if (guess.join('').toString() == this.state.word) {
+                this.setState({ guess: [], completed: true })
+            } else {
+                this.setState({ guess: [], attempt: this.state.attempt + 1 })
+            }
+        }
+    }
     render() {
+        console.log(this.state);
         return (
             <div>
-                {Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} attempt={this.state.attempt}
+                {this.state.chars.map((c, i) => <CharacterCard value={c} key={i} attempt={this.state.attempt}
                     activationHandler={this.activationHandler} />)}
             </div>
         );
