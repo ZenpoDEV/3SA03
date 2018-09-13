d0173919cfc92b6e6f046445814d1b8bc7a2f196
ZenpoDEV <krittamet0404@gmail.com>
2018-09-13 10:24:31 +0700

    intial

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CharacterCard from './CharacterCard.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Computer Engineering ,PSU Phuket Campus</h1>
+        <CharacterCard value="h" />
+        <CharacterCard value="i" />
       </div>
     );
   }
 }
-
 export default App;
