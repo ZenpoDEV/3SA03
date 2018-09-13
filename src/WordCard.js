61ecab852086caebea94cce10fd06db2f246ccb2
ZenpoDEV <krittamet0404@gmail.com>
2018-09-13 11:17:47 +0700

    Hadler as Props

--- src/WordCard.js
+++ src/WordCard.js
@@ -3,12 +3,20 @@ import './App.css';
 import CharacterCard from './CharacterCard.js';
 
 export default class WordCard extends
-Component {
-render() {
- return (
- <div>
-{ Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
- </div>
- );
- }
+    Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            attempt: '',
+        }
+    }
+    activationHandler = c => { console.log(`${c} has been activated.`) }
+    render() {
+        return (
+            <div>
+                {Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} attempt={this.state.attempt}
+                    activationHandler={this.activationHandler} />)}
+            </div>
+        );
+    }
 }
