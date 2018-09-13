1f695c3bddace860dd4610fcac873544c4dfae92
ZenpoDEV <krittamet0404@gmail.com>
2018-09-13 10:58:36 +0700

    HelloMore

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import CharacterCard from './CharacterCard.js';
-
+import WordCard from './WordCard.js';
 
 const word = "Hello";
 class App extends Component {
   render() {
     return (
       <div>
-        {
-          Array.from(word).map((c, i) => <CharacterCard value={c} key={i} />)
-        }
+        <WordCard value="hello"/>
       </div>
     );
   }
